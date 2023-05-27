d8f55dab1f5e94e0e99dbfc5703fa5018c0587d5
Lucas Wilmart <wilmart.lucas@gmail.com>
2023-05-28 00:48:46 +0200

    fix

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -4,7 +4,7 @@ import styled from 'styled-components'
 const Loader: React.FC = () => {
   return (
     <LoaderContainer>
-      <img src="/loading.gif" alt="loading..." />
+      <img src="./loading.gif" alt="loading..." />
     </LoaderContainer>
   )
 }
