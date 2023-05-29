78561975418e5c63ea001e7e9e16e22aa91a69d7
Lucas Wilmart <wilmart.lucas@gmail.com>
2023-05-29 13:52:13 +0200

    fix deploy

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import styled from 'styled-components'
 
-import EarthSvg from '@/../earth-icon.svg'
+import EarthSvg from '@/assets/images/earth-icon.svg'
 
 import routes from '@routes'
 
